fc822c3605f5014c8008136d116b4304845dbb53
Alok Kushwaha <94324872+Alok0818@users.noreply.github.com>
2023-01-21 18:08:28 +0530

    fixing error

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./Login.css";
 import logo from "../../assets/logo.svg";
 import { Select } from "@chakra-ui/react";
-import { Input } from '@chakra-ui/react'
-import { Button} from '@chakra-ui/react'
-import { useNavigate } from 'react-router-dom';
+import { Input } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -20,15 +20,26 @@ const Login = () => {
         </div>
         <div className="login-box">
           <div className="login-title">Sign-in to your account</div>
-          <Input placeholder='Username' mt="16px" />
-          <Input placeholder='Password' mt="16px" />
+          <Input placeholder="Username" mt="16px" />
+          <Input type="password" placeholder="Password" mt="16px" />
           <Select placeholder="Select Unit Name" mt="16px">
             <option value="option1">Option 1</option>
             <option value="option2">Option 2</option>
             <option value="option3">Option 3</option>
           </Select>
-          <Button onClick={() => navigate("/dashboard")} colorScheme='blue' rounded="12px" mt="48px" w="293px" bg="rgba(252, 105, 100, 1)" _hover={{ bg: "rgba(252, 105, 100, 1)" }}>Sign In</Button>
+          <Button
+            onClick={() => navigate("/dashboard")}
+            colorScheme="blue"
+            rounded="12px"
+            mt="48px"
+            w="293px"
+            bg="rgba(252, 105, 100, 1)"
+            _hover={{ bg: "rgba(252, 105, 100, 1)" }}
+          >
+            Sign In
+          </Button>
         </div>
+        inp
       </div>
     </div>
   );
